437fb1018d512a3c7d06d824e1fadf0d36cdb393
Erin M <erin.michon@gmail.com>
2021-12-16 23:05:02 -0500

    Created DB queries required for the object

--- server.js
+++ server.js
@@ -21,8 +21,37 @@ const db = mysql.createConnection(
   console.log('Connected to the election database.')
 )
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
+// GET a list of all potential candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+//GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err,row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+//Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err,result) => {
+//   if(err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// })
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const parms = [12, 'Erin', 'Michon', 1];
+
+db.query(sql, parms, (err, result) => {
+  if (err){
+    console.log(err);
+  }
+  console.log(result);
 });
 
 // Default response for any other request (Not Found)
